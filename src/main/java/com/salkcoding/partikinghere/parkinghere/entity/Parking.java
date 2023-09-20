cb39851ce3e035f9d336738e7f6e21eab533c2c7
Sangbeom Jo <josangbeom1104@gmail.com>
2023-09-21 01:08:51 +0900

    Data update

--- src/main/java/com/salkcoding/partikinghere/parkinghere/entity/Parking.java
+++ src/main/java/com/salkcoding/partikinghere/parkinghere/entity/Parking.java
@@ -14,88 +14,136 @@ import lombok.ToString;
 public class Parking {
 
     @Id
-    Long code;
+    Long parking_code;
 
-    @Column(name = "name")
-    String name;
+    @Column(name = "parking_name")
+    String parking_name;
 
-    @Column(name = "address")
-    String address;
+    @Column(name = "addr")
+    String addr;
 
-    @Column(name = "type")
-    String type;
+    @Column(name = "parking_type")
+    String parking_type;
 
-    @Column(name = "operateClassification")
-    String operateClassification;
+    @Column(name = "operation_rule_nm")
+    String operation_rule_nm;
 
-    @Column(name = "phoneNumber")
-    String phoneNumber;
+    @Column(name = "tel")
+    String tel;
 
-    @Column(name = "syncState")
-    String syncState;
+    @Column(name = "que_status_nm")
+    String que_status_nm;
 
-    @Column(name = "totalParkingSpot")
-    String totalParkingSpot;
+    @Column(name = "capacity")
+    Integer capacity;
 
-    @Column(name = "paidOrFree")
-    String paidOrFree;
+    @Column(name = "cur_parking")
+    Integer cur_parking;
 
-    @Column(name = "nightOpen")
-    String nightOpen;
+    @Column(name = "pay_nm")
+    String pay_nm;
 
-    @Column(name = "openWeekdayTime")
-    Integer openWeekdayTime;
+    @Column(name = "day_maximum")
+    Integer day_maximum;
 
-    @Column(name = "closeWeekdayTime")
-    Integer closeWeekdayTime;
+    @Column(name = "add_rates")
+    Integer add_rates;
 
-    @Column(name = "openWeekendTime")
-    Integer openWeekendTime;
+    @Column(name = "bus_add_rates")
+    Integer bus_add_rates;
 
-    @Column(name = "closeWeekendTime")
-    Integer closeWeekendTime;
+    @Column(name = "lat")
+    Float lat;
 
-    @Column(name = "openHolidayTime")
-    Integer openHolidayTime;
+    @Column(name = "lng")
+    Float lng;
 
-    @Column(name = "closeHolidayTime")
-    Integer closeHolidayTime;
+    @Column(name = "holiday_pay_yn")
+    String holiday_pay_yn;
 
-    @Column(name = "lastSyncTime")
-    String lastSyncTime;
+    @Column(name = "bus_rates")
+    String bus_rates;
 
-    @Column(name = "paidOrFreeOnWeekend")
-    String paidOrFreeOnWeekend;
+    @Column(name = "que_status")
+    Integer que_status;
 
-    @Column(name = "paidOrFreeOnHoliday")
-    String paidOrFreeOnHoliday;
+    @Column(name = "cur_parking_time")
+    String cur_parking_time;
 
-    @Column(name = "subscribePrice")
-    Integer subscribePrice;
+    @Column(name = "sh_type")
+    String sh_type;
 
-    @Column(name = "managementNumber")
-    String managementNumber;
+    @Column(name = "weekday_begin_time")
+    Integer weekday_begin_time;
 
-    @Column(name = "defaultParkingPrice")
-    Integer defaultParkingPrice;
+    @Column(name = "time_rate")
+    Integer time_rate;
 
-    @Column(name = "defaultParkingTime")
-    Integer defaultParkingTime;
+    @Column(name = "sh_co")
+    String sh_co;
 
-    @Column(name = "additionalParkingPrice")
-    Integer additionalParkingPrice;
+    @Column(name = "night_free_open_nm")
+    String night_free_open_nm;
 
-    @Column(name = "additionalParkingTime")
-    Integer additionalParkingTime;
+    @Column(name = "add_time_rate")
+    Integer add_time_rate;
 
-    @Column(name = "maxPriceOnDay")
-    Integer maxPriceOnDay;
+    @Column(name = "sh_etc")
+    String sh_etc;
 
-    @Column(name = "latitude")
-    Float latitude;
+    @Column(name = "rates")
+    Integer rates;
 
-    @Column(name = "longitude")
-    Float longitude;
+    @Column(name = "weekend_begin_time")
+    Integer weekend_begin_time;
+
+    @Column(name = "weekday_end_time")
+    Integer weekday_end_time;
+
+    @Column(name = "parking_type_nm")
+    String parking_type_nm;
+
+    @Column(name = "saturday_pay_yn")
+    String saturday_pay_yn;
+
+    @Column(name = "operation_rule")
+    Integer operation_rule;
+
+    @Column(name = "bus_add_time_rate")
+    Integer bus_add_time_rate;
+
+    @Column(name = "weekend_end_time")
+    Integer weekend_end_time;
+
+    @Column(name = "pay_yn")
+    String pay_yn;
+
+    @Column(name = "sh_link")
+    String sh_link;
+
+    @Column(name = "holiday_pay_nm")
+    String holiday_pay_nm;
+
+    @Column(name = "saturday_pay_nm")
+    String saturday_pay_nm;
+
+    @Column(name = "holiday_end_time")
+    Integer holiday_end_time;
+
+    @Column(name = "fulltime_monthly")
+    Integer fulltime_monthly;
+
+    @Column(name = "grp_parknm")
+    Integer grp_parknm;
+
+    @Column(name = "night_free_open")
+    String night_free_open;
+
+    @Column(name = "bus_time_rate")
+    Integer bus_time_rate;
+
+    @Column(name = "holiday_begin_time")
+    Integer holiday_begin_time;
 
     public Parking() {
 
